55365e1f11693a3a852ac0b6a2820fb759fde6a7
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2024-04-09 23:21:43 +0300

    tailwind sidebar title btn-AddProject

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,9 @@
 export default function SideBar() {
     return (
-        <div className="fixed top-15  w-64 h-screen rounded-tr-[20px]  bg-stone-800">
-            <span className=" text-white left-15">your projects</span>
-            <button>+ Add Project</button>
-        </div>
+        <aside className="fixed top-16 h-screen w-64 rounded-tr-[20px] bg-stone-900">
+            <label className="relative uppercase left-8 top-8 font-sans text-2xl font-bold text-white ">Your projects</label>
+            <button className="relative text-black bg-neutral-700 top-16 left-8  rounded-md py-2 px-4 hover:bg-neutral-600 hover:text-zinc-400 ">+ Add Project</button>
+
+        </aside>
     );
 }
